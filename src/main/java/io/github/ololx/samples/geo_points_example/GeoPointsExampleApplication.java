041f3ee7db517aeeda79e1fdb3146a2d5d6e3071
a-kropotin <a.kropotin@pixonic.com>
2023-10-11 11:31:32 +0300

    chore(*) init project

---
+++ src/main/java/io/github/ololx/samples/geo_points_example/GeoPointsExampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.ololx.samples.geo_points_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoPointsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoPointsExampleApplication.class, args);
+	}
+}
---
+++ src/main/java/io/github/ololx/samples/geo_points_example/controller/PointTranslationController.java
@@ -0,0 +1,9 @@
+package io.github.ololx.samples.geo_points_example.controller;
+
+/**
+ * @author Alexander A. Kropotin
+ *     project geo-points-example
+ *     created 11/10/2023 11:30 am
+ */
+public class PointTranslationController {
+}

470a71cdc515c52bf9c21d843ceb649e9da1378e
a-kropotin <a.kropotin@pixonic.com>
2023-10-15 22:06:34 +0300

    chore(PointTransformationController.java) rewrite on CFs

--- src/main/java/io/github/ololx/samples/geo_points_example/controller/PointTransformationController.java
+++ src/main/java/io/github/ololx/samples/geo_points_example/controller/PointTransformationController.java
@@ -4,16 +4,22 @@ import io.github.ololx.moonshine.tuple.Couple;
 import io.github.ololx.samples.geo_points_example.model.PointTransformationFromModel;
 import io.github.ololx.samples.geo_points_example.translation.PointTransformationStrategy;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.threads.VirtualThreadExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author Alexander A. Kropotin
- * project geo-points-example
- * created 11/10/2023 11:30 am
+ *     project geo-points-example
+ *     created 11/10/2023 11:30 am
  */
 @RequiredArgsConstructor
 @Controller
@@ -22,36 +28,52 @@ public class PointTransformationController {
     private final PointTransformationStrategy pointTranslationStrategy;
 
     @GetMapping({"/", "/index", "/transformation"})
-    public String index(Model model) {
-        model.addAttribute("pointTransformationRequest", PointTransformationFromModel.empty());
+    public CompletableFuture<String> index(Model model) {
+        model.addAttribute("pointTransformationModel", PointTransformationFromModel.empty());
 
-        return "transformation";
+        return CompletableFuture.completedFuture("transformation");
     }
 
     @PostMapping("/transformation")
-    public String transform(@ModelAttribute PointTransformationFromModel pointTransformationRequest, Model model) {
-        PointTransformationFromModel transformationRequesrParams = (PointTransformationFromModel) model.getAttribute("pointTransformationFromModel");
+    public CompletableFuture<String> transform(@ModelAttribute PointTransformationFromModel pointTransformationRequest,
+                                               Model model) {
+        PointTransformationFromModel transformationRequestParams = (PointTransformationFromModel) model.getAttribute(
+            "pointTransformationFromModel");
 
-        var newPointCoords = this.pointTranslationStrategy.transform(
-            Couple.of(transformationRequesrParams.getX(), transformationRequesrParams.getY()),
-            transformationRequesrParams.getFromCrs(),
-            transformationRequesrParams.getToCrs()
-        );
+        if (transformationRequestParams == null) {
+            return CompletableFuture.completedFuture("transformation");
+        }
 
-        model.addAttribute(
-            "pointTransformationRequest",
-            new PointTransformationFromModel(
-                transformationRequesrParams.getX(),
-                transformationRequesrParams.getY(),
-                transformationRequesrParams.getZ(),
-                newPointCoords.getT0(),
-                newPointCoords.getT1(),
-                transformationRequesrParams.getZ(),
-                transformationRequesrParams.getFromCrs(),
-                transformationRequesrParams.getToCrs()
-            )
-        );
+        return CompletableFuture.supplyAsync(() -> {
+                var newPointCoords = this.pointTranslationStrategy.transform(
+                    Couple.of(transformationRequestParams.getX(), transformationRequestParams.getY()),
+                    transformationRequestParams.getFromCrs(),
+                    transformationRequestParams.getToCrs()
+                );
 
-        return "transformation";
+                model.addAttribute(
+                    "pointTransformationModel",
+                    new PointTransformationFromModel(
+                        transformationRequestParams.getX(),
+                        transformationRequestParams.getY(),
+                        transformationRequestParams.getZ(),
+                        newPointCoords.getT0(),
+                        newPointCoords.getT1(),
+                        transformationRequestParams.getZ(),
+                        transformationRequestParams.getFromCrs(),
+                        transformationRequestParams.getToCrs()
+                    )
+                );
+
+                return "transformation";
+            })
+            .handleAsync(((result, throwable) -> {
+                if (throwable != null) {
+                    model.addAttribute("pointTransformationModel", PointTransformationFromModel.empty());
+                    model.addAttribute("errorMessage", "The specified CRS doesn't exists");
+                }
+
+                return result;
+            }));
     }
 }
